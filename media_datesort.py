bb80dbb71f652f7d01b05ca78f8a16ed0bbb75e3
mag37 <robin.ivehult@gmail.com>
2023-10-14 20:33:46 +0200

    removed mp4

--- media_datesort.py
+++ media_datesort.py
@@ -7,7 +7,7 @@ import logging
 # User parameters:
 input_path = "./Testing/Input"
 output_path = "./Testing/Output"
-file_types = (".jpeg", ".jpg", ".png", ".mp4", ".dng")
+file_types = (".jpeg", ".jpg", ".png", ".dng")
 no_date_dir = "NoDate" # name of the directory to store no-dated images
 date_separator = "-" # character to separate dates, eg. 2023-01-01 or 2023.01.01 DONT use slashes
 
