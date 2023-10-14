5a2f62b6682bd4f9372cc8e85e8d20165e5ffa56
mag37 <robin.ivehult@gmail.com>
2023-10-14 20:08:35 +0200

    pushing from local

---
+++ extras/date_from_filename.py
@@ -0,0 +1,42 @@
+import re
+def extract_date(filename):
+    date_pattern = re.compile(r'\d{2,4}[-_.]\d{2}[-_.]\d{2,4}')
+    match = date_pattern.search(filename)
+    if match:
+        return match.group()
+    else:
+        return None
+
+filename = 'my_report_2022-03-21.pdf'
+date = extract_date(filename)
+
+if date:
+    print(f'Date found in filename: {date}')
+else:
+    print('No valid date found in filename.')
+
+
+# Version 2
+
+from datetime import datetime
+
+def likely_date_format(filename):
+    date_formats = ['%y%m%d', '%Y%m%d', '%d%m%y', '%d%m%Y']
+    date_pattern = re.search(r'\d{6,8}', filename)
+    match = date_pattern.group(0)
+    if date_pattern:
+        for format in date_formats:
+            try: 
+                datetime.strptime(match, format)
+                return match, format
+            except ValueError:
+                pass
+    return None
+
+files = ["file1_230711.jpg", "file2_20230821.jpg", "file3-070923.jpg", "file4-990999.jpg"]
+for file in files:
+    print(file)
+    print(likely_date_format(file))
+
+
+
---
+++ media_datesort.py
@@ -0,0 +1,83 @@
+import os
+import shutil
+import filecmp
+import exifread
+import logging
+
+# User parameters:
+input_path = "./Testing/Input"
+output_path = "./Testing/Output"
+file_types = (".jpeg", ".jpg", ".png", ".mp4", ".dng")
+no_date_dir = "NoDate" # name of the directory to store no-dated images
+date_separator = "-" # character to separate dates, eg. 2023-01-01 or 2023.01.01 DONT use slashes
+
+# Logging format, handlers to write to file and console
+logging.basicConfig(
+        format="%(asctime)s %(levelname)s %(message)s", 
+        datefmt="%y/%m/%d %H:%M:%S", 
+        level=logging.INFO,
+        handlers=[
+            logging.FileHandler("debug.log"),
+            logging.StreamHandler() # writing to stderr
+            ]
+        )
+
+# Check user-defined paths:
+if not os.path.exists(input_path):
+    print("Input directory does not exist, quitting")
+    exit()
+if not os.path.exists(output_path):
+    print(f"Output directory does not exist - creating it @ {output_path}")
+    os.mkdir(output_path)
+
+def copy_file(src, dst):
+    try:
+        shutil.copy(src, dst)
+        logging.info(f"File copied. Source: {src} Destination: {dst}")
+    except Exception as e:
+        logging.error(f"Skipping file: {e}")
+
+# function to make unique names
+def unique_filename(dst):
+    suffix = 0
+    base, ext = os.path.splitext(dst)
+    while os.path.exists(dst):
+        dst = base + f"_{suffix}" + ext
+        suffix += 1
+    return dst
+
+def date_and_copy(file_path):
+    with open(file_path, "rb") as f:
+        filename = os.path.basename(file_path)
+        try:
+            # grab exif data:
+            exif_data = exifread.process_file(f, details=False, stop_tag="DateTimeOriginal")
+            # strip and reformat date from "2023:08:01 10:18:45" to "2023-08-01"
+            date_stamp = str(exif_data["EXIF DateTimeOriginal"])[:10].replace(":", date_separator) 
+            date_list = date_stamp.split(date_separator)
+            # Create full path of subdirectories ex: 2023/08/2023-08-01
+            output_dir = os.path.join(output_path, date_list[0], date_list[1], date_stamp)
+        except:
+            # if no date-exif, copy file to "no-date dir"
+            output_dir = os.path.join(output_path, no_date_dir)
+            logging.warning(f"No exif-data on file {filename}. Copying to {no_date_dir}")
+        src = file_path
+        dst = os.path.join(output_dir, filename)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)  # makedirs to be able to create the full path of subdirectories
+        if os.path.exists(dst): # check src+dst same name
+            if filecmp.cmp(src, dst): # check src+dst same content
+                logging.info(f"Skipping file: {dst} already exists")
+            else: 
+                dst = unique_filename(dst)
+                logging.info(f"Different content but same name, adding suffix {dst}")
+                copy_file(src, dst)
+        else:
+            copy_file(src, dst)
+
+# Iterate over all files and subdirectories
+for root, dirs, files in os.walk(input_path):
+    for file in files:
+        # check extension - "lower" to be case-insensitive
+        if file.lower().endswith(file_types):
+            date_and_copy(os.path.join(root, file))
