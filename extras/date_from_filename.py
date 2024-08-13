075f49ebf61803e192309291268d5b490591d5ea
mag37 <robin.ivehult@gmail.com>
2024-08-13 22:04:45 +0200

    continuing date-guess script

--- extras/date_from_filename.py
+++ extras/date_from_filename.py
@@ -1,44 +1,27 @@
 import re
 from datetime import datetime
 
-## Version 1
-# def extract_date(filename):
-#     date_pattern = re.compile(r'\d{2,4}[-_.]\d{2}[-_.]\d{2,4}')
-#     match = date_pattern.search(filename)
-#     if match:
-#         return match.group()
-#     else:
-#         return None
-
-# filename = 'my_report_2022-03-21.pdf'
-# date = extract_date(filename)
-
-# if date:
-#     print(f'Date found in filename: {date}')
-# else:
-#     print('No valid date found in filename.')
-
-
-## Version 2
-
-
 def likely_date_format(filename):
-    date_formats = ['%y%m%d', '%Y%m%d', '%d%m%y', '%d%m%Y']
-    date_pattern = re.search(r'\d{6,8}', filename)
-    match = date_pattern.group(0)
-    if date_pattern:
+    date_formats = ['%y%m%d', '%Y%m%d', '%d%m%y', '%d%m%Y', '%y-%m-%d', '%Y-%m-%d', '%d-%m-%y', '%d-%m-%Y', '%y.%m.%d', '%Y.%m.%d', '%d.%m.%y', '%d.%m.%Y']
+    date_regex = [r'\d{6,8}', r'\d{2,4}-\d{2}-\d{2}', r'\d{2,4}\.\d{2}\.\d{2}']
+    #date_pattern = re.search(r'\d{6,8}', filename)
+    regex = r'|'.join(date_regex)
+    date_pattern = re.search(regex, filename)
+    if date_pattern: # to check if pattern is found, else return None
+        match = date_pattern.group(0)
         for format in date_formats:
-            try: 
-                datetime.strptime(match, format)
+            try:
+                # Check if match is valid date, strftime() is required due to strptime() dont check zero padded month/day
+                if match != datetime.strptime(match, format).strftime(format):
+                    raise ValueError
                 return match, format
             except ValueError:
-                pass
+              pass
     return None
 
-files = ["file1_230711.jpg", "file2_20230821.jpg", "file3-070923.jpg", "file4-990999.jpg", "20231023_111211_946_img.jpg", "20230804_203421_960_img.jpg"]
+files = [ "invalid1_1234.jpg", "invalid2-999999.jpg", "file1_230711.jpg", "file2_20230821.jpg", "20231023_111211_946_img.jpg", "20230804_203421_960_img.jpg", "24-07-01_test1.jpg", "2024.07.02_test2.jpg"]
 for file in files:
     print(file)
     print(likely_date_format(file))
-
 
 
