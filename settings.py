36f2baea656c1c809bfb8aa44d52ee2c21fe4ad0
mag37 <robin.ivehult@gmail.com>
2026-07-15 22:08:13 +0200

    made filepath variables be read as raw strings

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-input_path = "./Testing/Input"
-output_path = "./Testing/Output"
+input_path = r"./Testing/Input"
+output_path = r"./Testing/Output"
 file_types = (".jpeg", ".jpg", ".png", ".dng")
 no_date_dir = "NoDate" # name of the directory to store images with no exif-date
 unsortable_dir = "Unsortables" # name of the directory to store images that cant be guessed
